2f119073519731bc32e67de852afe8efe1c8deb6
Vladyslav <pro@Mac-M3-Pro.local>
2025-07-30 21:41:55 -0700

    🎉 init: initial project setup

---
+++ HabitHonker/HabitHonker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  HabitHonker
+//
+//  Created by Vladyslav on 7/30/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
